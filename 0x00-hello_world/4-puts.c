b1a440ae55a9bfb6a9ed9a92aacc9c080be0b976
minenhle-8201 <minenhle8201@gmail.com>
2023-06-16 05:59:16 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,5 +6,5 @@
  */
 int main(void)
 {
-	puts('''\''Programming is like building a multilingual puzzle");
+	puts('''\"Programming is like building a multilingual puzzle");
 }
