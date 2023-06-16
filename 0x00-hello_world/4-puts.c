fdbd20d9e58d7a1821da264d13f5ac90239a6314
minenhle-8201 <minenhle8201@gmail.com>
2023-06-16 02:26:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
-#include <stdio.h>
+# include <stdio.h>
 /**
- * main - Entry point 
+ * main-Entry point 
  *
- * Return: Always  (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	put('"\"Programming is like building a multilingual puzzle");
+	put('''\''Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
